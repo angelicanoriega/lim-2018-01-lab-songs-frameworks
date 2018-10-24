4d916369c0bcdbea78bf2aba1e25557fcbf41884
Maria <mariaanguevara@gmail.com>
2018-10-24 15:26:07 -0500

    Updates

--- precache-manifest.d6f7dfa4de44ed3730fc0165f6f6525d.js
+++ precache-manifest.d6f7dfa4de44ed3730fc0165f6f6525d.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/lim-2018-01-lab-songs-frameworks/static/js/runtime~main.45d975ff.js"
   },
   {
-    "revision": "1194f544c99e07b83def",
-    "url": "/lim-2018-01-lab-songs-frameworks/static/js/main.1194f544.chunk.js"
+    "revision": "28001b2d57a87dd7d950",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/main.28001b2d.chunk.js"
   },
   {
     "revision": "8df7078f39324016d415",
     "url": "/lim-2018-01-lab-songs-frameworks/static/js/1.8df7078f.chunk.js"
   },
   {
-    "revision": "1194f544c99e07b83def",
+    "revision": "28001b2d57a87dd7d950",
     "url": "/lim-2018-01-lab-songs-frameworks/static/css/main.877b896c.chunk.css"
   },
   {
-    "revision": "8033bb06d673408ca809e519372c4507",
+    "revision": "e105abadab038d5a80bbc6d115cd06c5",
     "url": "/lim-2018-01-lab-songs-frameworks/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/lim-2018-01-lab-songs-frameworks/precache-manifest.b539826fd4ca215eb5c661f4e1317c66.js"
+  "/lim-2018-01-lab-songs-frameworks/precache-manifest.d6f7dfa4de44ed3730fc0165f6f6525d.js"
 );
 
 workbox.clientsClaim();
