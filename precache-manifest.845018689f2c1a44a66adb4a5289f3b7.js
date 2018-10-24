c9bb7188afa5e04c173e1cae1b3a0f8e5da3580c
Maria <mariaanguevara@gmail.com>
2018-10-24 14:19:40 -0500

    Updates

---
+++ precache-manifest.845018689f2c1a44a66adb4a5289f3b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "45d975ffd74dd0f719d3",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/runtime~main.45d975ff.js"
+  },
+  {
+    "revision": "6485fd8d816850b7b5a0",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/main.6485fd8d.chunk.js"
+  },
+  {
+    "revision": "8df7078f39324016d415",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/1.8df7078f.chunk.js"
+  },
+  {
+    "revision": "6485fd8d816850b7b5a0",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/css/main.34b14f5e.chunk.css"
+  },
+  {
+    "revision": "2412edeb8484c3b55415b34bd8973996",
+    "url": "/lim-2018-01-lab-songs-frameworks/index.html"
+  }
+];
